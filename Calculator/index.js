ae39019a847599dab13b279081be285683824b62
Munir0000 <muniralifordev1@gmail.com>
2024-09-30 06:20:06 +0300

    i've added the code that handles all operations in the Calculator

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,4 +1,46 @@
-const pads = document.querySelectorAll(".pads");
-pads.forEach((pad, index) => {
-  pad.textContent = index + 1;
+const pads = document.querySelectorAll(".padsContainer .btn");
+const screen = document.querySelector("span");
+const btnclear = document.querySelector("#one");
+let resultArray = [];
+
+btnclear.addEventListener("click", () => {
+  screen.textContent = "0";
+  resultArray = [];
+});
+
+pads.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.textContent;
+
+    if (!isNaN(value) || value === ".") {
+      screen.textContent =
+        screen.textContent === "0" ? value : screen.textContent + value;
+    }
+
+    if (value === "+" || value === "−" || value === "×" || value === "÷") {
+      resultArray.push(parseFloat(screen.textContent));
+      resultArray.push(value);
+      screen.textContent = "";
+    }
+
+    if (value === "=") {
+      resultArray.push(parseFloat(screen.textContent));
+      let result = resultArray.reduce((acc, cur, index, arr) => {
+        if (typeof cur === "number") {
+          return acc;
+        } else if (cur === "+") {
+          return acc + arr[index + 1];
+        } else if (cur === "−") {
+          return acc - arr[index + 1];
+        } else if (cur === "×") {
+          return acc * arr[index + 1];
+        } else if (cur === "÷") {
+          return acc / arr[index + 1];
+        }
+      }, resultArray[0]);
+
+      screen.textContent = result;
+      resultArray = [];
+    }
+  });
 });
